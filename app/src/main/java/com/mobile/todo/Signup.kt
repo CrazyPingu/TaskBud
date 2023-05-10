833b3698146f14799c47b5ba08df258e0307b261
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-05-10 17:51:32 +0200

    Add signup frontend

---
+++ app/src/main/java/com/mobile/todo/Signup.kt
@@ -0,0 +1,12 @@
+package com.mobile.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Signup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
