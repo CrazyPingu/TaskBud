376c82a5f4774886cda0314cacc480bb4dcb4847
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-05-10 18:13:15 +0200

    Add redirect from signup to login and viceversa

--- app/src/main/java/com/mobile/todo/MainActivity.kt
+++ app/src/main/java/com/mobile/todo/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.mobile.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var username : EditText? = null
+    private var signupButton : Button? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.username)
+        signupButton = findViewById(R.id.signup_button)
+
+        // Redirect to Signup Activity
+        signupButton?.setOnClickListener {
+            startActivity(Intent(this, Signup::class.java))
+        }
     }
 }
--- app/src/main/java/com/mobile/todo/Signup.kt
+++ app/src/main/java/com/mobile/todo/Signup.kt
@@ -1,12 +1,23 @@
 package com.mobile.todo
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class Signup : AppCompatActivity() {
 
+    private var loginButton : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        loginButton = findViewById(R.id.login_button)
+
+        // Redirect to Login Activity
+        loginButton?.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
