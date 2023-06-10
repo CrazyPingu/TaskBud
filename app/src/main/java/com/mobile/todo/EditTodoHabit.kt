2698d1bb0cfd858f2de652eb1330c54f570f8c2d
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-06-10 16:53:28 +0200

    Add date dialog

--- app/src/main/java/com/mobile/todo/EditTodoHabit.kt
+++ app/src/main/java/com/mobile/todo/EditTodoHabit.kt
@@ -1,12 +1,32 @@
 package com.mobile.todo
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class EditTodoHabit : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text)
+
+        val dateButton = findViewById<Button>(R.id.date)
+
+        dateButton.setOnClickListener {
+            // Get current date
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            // Date Picker Dialog
+            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in text view
+                dateButton.text = "$dayOfMonth/${monthOfYear + 1}/$year"
+            }, year, month, day)
+            dpd.show() // Show DatePickerDialog
+        }
     }
 }
