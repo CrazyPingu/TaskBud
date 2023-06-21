4544071adf2f245cf455d2ee66567e528d7977a3
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-06-21 16:54:49 +0200

    Fix change fragment todo

--- app/src/main/java/com/mobile/todo/fragment/TodoPage.kt
+++ app/src/main/java/com/mobile/todo/fragment/TodoPage.kt
@@ -1,62 +1,55 @@
 package com.mobile.todo.fragment
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.*
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-//import androidx.recyclerview.widget.RecyclerView
 import com.mobile.todo.R
-import com.mobile.todo.adapter.TodoAdapter
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.mobile.todo.database.AppDatabase
 
 
 class TodoPage : Fragment() {
 
     private var USER_ID: Int = 0
     private lateinit var searchView: SearchView
-    //private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: TodoAdapter
     private lateinit var arrayAdapter: ArrayAdapter<String>
+    private lateinit var handler: Handler
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_todo, container, false)
 
-        val searchView = view.findViewById<SearchView>(R.id.search_view)
+        searchView = view.findViewById(R.id.search_view)
         val listView = view.findViewById<ListView>(R.id.list_search_view)
-
-        val yourDao = AppDatabase.getDatabase(view.context).folderDao()
 
         val itemList = listOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j")
 
-        arrayAdapter = ArrayAdapter<String>(view.context, android.R.layout.simple_list_item_1, itemList)
+        arrayAdapter = ArrayAdapter(view.context, android.R.layout.simple_list_item_1, itemList)
 
-        listView.setOnItemClickListener { parent, view, position, id ->
-            val item = parent.getItemAtPosition(position) as String
-            if(item.isNotEmpty()) {
-                Log.d("search", item)
-            }
+        listView.setOnItemClickListener { _, _, position, _ ->
+            val item = arrayAdapter.getItem(position) as String
+            Log.d("ListClick", item)
         }
         listView.adapter = arrayAdapter
 
+        handler = Handler(Looper.getMainLooper())
+
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                // you can handle query submit here if you want
+                // Handle query submit if needed
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                // here you can execute a query every time a character is written
-                Log.d("search", newText.toString())
-                arrayAdapter.filter.filter(newText)
+                handler.post {
+                    // Execute query when text changes
+                    arrayAdapter.filter.filter(newText)
+                }
                 return true
             }
         })
