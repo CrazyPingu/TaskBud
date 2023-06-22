a927b459af0cae05ee871a9164631d2c04205285
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-06-22 15:31:49 +0200

    Update todo db

--- app/src/main/java/com/mobile/todo/database/dao/ToDoDao.kt
+++ app/src/main/java/com/mobile/todo/database/dao/ToDoDao.kt
@@ -3,10 +3,25 @@ package com.mobile.todo.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.mobile.todo.database.dataset.Habit
 import com.mobile.todo.database.dataset.ToDo
+import java.util.Date
 
 @Dao
 interface ToDoDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insertFolder(toDo: ToDo)
+
+    @Query("SELECT * FROM todo where userId = :userId")
+    fun getAllToDoByUserId(userId: Int): List<ToDo>
+
+    @Query("SELECT * FROM todo where userId = :userId and folderId = :folderId")
+    fun getAllToDoByTagId(userId: Int, folderId: Int): List<ToDo>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertToDo(todo: ToDo)
+
+    @Query("UPDATE todo SET title = :title, description = :description, date = :date, completed = :completed, folderId = :folderId WHERE id = :id")
+    fun updateToDo(id: Int, title: String, description: String, date: String, completed: Boolean, folderId: Int)
 }
