9cff05e9581ec9b0700ec7d6c9ba0b490b9e3b4e
Sara Romeo <sara.romeo3@studio.unibo.it>
2023-06-22 17:09:35 +0200

    Update Tag.kt

--- app/src/main/java/com/mobile/todo/database/dataset/Tag.kt
+++ app/src/main/java/com/mobile/todo/database/dataset/Tag.kt
@@ -1,21 +1,11 @@
 package com.mobile.todo.database.dataset
 
 import androidx.room.Entity
-import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity(
-    foreignKeys = [ForeignKey(
-        entity = User::class,
-        parentColumns = arrayOf("id"),
-        childColumns = arrayOf("userId"),
-        onUpdate = ForeignKey.CASCADE,
-        onDelete = ForeignKey.CASCADE
-    )]
-)
+@Entity
 data class Tag(
     val name: String,
-    val userId: Int,
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 )
