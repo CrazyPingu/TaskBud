0d24b5944daaeeb08e38a3ba950c74ba6ad436d8
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-06-26 15:31:04 +0200

    Removing habit stats page

--- app/src/main/java/com/mobile/todo/fragment/ProfilePage.kt
+++ app/src/main/java/com/mobile/todo/fragment/ProfilePage.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.mobile.todo.Camera
 import com.mobile.todo.HabitStats
 import com.mobile.todo.HomePage
@@ -19,6 +20,9 @@ import com.mobile.todo.utils.Constant
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.eazegraph.lib.charts.PieChart
+import org.eazegraph.lib.models.PieModel
 import java.io.IOException
 import java.io.InputStream
 
@@ -32,10 +36,6 @@ class ProfilePage : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
-
-        view.findViewById<Button>(R.id.habits_stats).setOnClickListener {
-            startActivity(Intent(context, HabitStats::class.java))
-        }
 
         GlobalScope.launch {
             val user = AppDatabase.getDatabase(requireContext()).userDao().getUser(HomePage.USER_ID)
@@ -73,6 +73,56 @@ class ProfilePage : Fragment() {
                     intent.putExtra("profilePic", profilePicUri)
                     Camera.PAGE_TO_RETURN = HomePage::class
                     startActivity(intent)
+                }
+            }
+        }
+
+
+        val mPieChart = view.findViewById<PieChart>(R.id.piechart)
+
+        // number1 is the total size of the pie.
+        // number2 is the space occupied.
+        val database = AppDatabase.getDatabase(requireContext())
+
+        GlobalScope.launch(Dispatchers.IO) {
+            val completed = database.habitDao().getCompletedHabitsCount(HomePage.USER_ID).toFloat()
+            val total = database.habitDao().getUserHabitsCount(HomePage.USER_ID).toFloat()
+
+            // The rest of the pie that is not occupied.
+            val notCompleted = total - completed
+
+            withContext(Dispatchers.Main) {
+
+                if(total != 0f) {
+                    // Create slices for the PieChart.
+                    mPieChart.addPieSlice(
+                        PieModel(
+                            "Completed",
+                            completed,
+                            ContextCompat.getColor(requireContext(), R.color.piechart_completed)
+                        )
+                    )
+                    mPieChart.addPieSlice(
+                        PieModel(
+                            "Not completed",
+                            notCompleted,
+                            ContextCompat.getColor(requireContext(), R.color.piechart_incompleted)
+                        )
+                    )
+
+                    // Remove the inner padding to make the chart a full pie.
+                    mPieChart.innerPadding = 10f
+
+                    // Set the animation time to 500 milliseconds.
+                    mPieChart.animationTime = 500
+
+                    // Draw the pie chart.
+                    mPieChart.startAnimation()
+
+                }else{
+                    view.findViewById<TextView>(R.id.completed).visibility = View.GONE
+                    view.findViewById<TextView>(R.id.incompleted).visibility = View.GONE
+                    view.findViewById<TextView>(R.id.no_data_message).visibility = View.VISIBLE
                 }
             }
         }
